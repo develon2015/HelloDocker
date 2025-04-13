c3f87301842bb19677413f5675004df365f5b077
Develon2015 <302615249@qq.com>
2025-04-13 17:20:28 +0800

    rust

--- Ubuntu-Setup/setup.sh
+++ Ubuntu-Setup/setup.sh
@@ -1,11 +1,11 @@
+function rust() {
+    snap install rustup --classic
+    rustup default stable
+}
+
 function main() {
   rust
   echo "OK"
 }
 
 main
-
-function rust() {
-    snap install rustup --classic
-    rustup default stable
-}
