560696feebe8fadeb7d99e373b6a38a57a774b16
Develon2015 <302615249@qq.com>
2025-04-13 17:49:07 +0800

    Node.js

--- Ubuntu-Setup.sh
+++ Ubuntu-Setup.sh
@@ -1,5 +1,5 @@
 apt update
 
-apt install tmux wget -y
+apt install tmux wget curl -y
 
 tmux new -s setup 'bash -c "wget https://github.com/develon2015/HelloDocker/raw/refs/heads/main/Ubuntu-Setup/setup.sh && bash setup.sh; bash"'
--- Ubuntu-Setup/setup.sh
+++ Ubuntu-Setup/setup.sh
@@ -1,4 +1,28 @@
-function rust() {
+# https://github.com/nvm-sh/nvm
+# Node 版本管理器 - 符合 POSIX 标准的 bash 脚本，用于管理多个活动的 node.js 版本
+function nvm_install() {
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
+}
+
+# https://github.com/Schniz/fnm
+# 快速、简单的 Node.js 版本管理器，采用 Rust 构建
+function fvm_install() {
+    curl -fsSL https://fnm.vercel.app/install | bash
+}
+
+# https://github.com/pkgxdev/pkgx
+# 强大的 pkgx 和 pkgm 命令，支持多种软件包命令，支持的软件包列表：https://pkgx.dev/pkgs/
+function pkgx_install() {
+    curl https://pkgx.sh | sh
+}
+
+# 安装 Node.js
+function nodejs_install() {
+    apt install nodejs npm -y
+}
+
+# 安装 Rust
+function rust_install() {
     snap install rustup --classic
     rustup default stable
 }
